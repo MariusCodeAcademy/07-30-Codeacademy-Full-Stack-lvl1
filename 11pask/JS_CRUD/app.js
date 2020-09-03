b3adbc0115a3f2b5a9b620955e0dee617e25b528
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2020-09-03 21:05:33 +0300

    Crud complete some refactoring done

--- 11pask/JS_CRUD/app.js
+++ 11pask/JS_CRUD/app.js
@@ -4,7 +4,7 @@ console.log('app.js');
 let todos = [
     { name: 'do stuff', done: true, edit: true },
     { name: 'Learn Html', done: false, edit: false },
-    { name: 'Style witn Css', done: true, edit: false },
+    { name: 'Style witn Css', done: true, edit: true },
     { name: 'Act with Js Forever', done: false, edit: false }
 
 ]
@@ -20,12 +20,40 @@ function addTodo(todoText) {
     todos.push({ name: todoText, done: false, edit: false });
     render();
 }
-
+// funkcijos aprasymas 
 function deleteTodo(id) {
     // istrynimas su splice
     todos.splice(id, 1);
     render();
 }
+
+function renderSpanOrInput(todoItem) {
+    if (todoItem.edit === true) {
+        // grazinti input
+        return `<input type="text" class="text" value="${todoItem.name}" >`;
+    } else {
+        // grazinti span
+        return `<span class="text ${handleTextChecked(todoItem)}">${todoItem.name}</span>`;
+    }
+}
+
+function handleClassCheck(currentTodo) {
+    return currentTodo.done ? 'fa-check-circle' : 'fa-circle-thin';
+}
+
+function handleTextChecked(currentTodo) {
+    if (currentTodo.done) return 'line-through';
+}
+
+function addValueToTodoList(myValue) {
+    if (myValue !== '') addTodo(myValue);
+    inputElement.value = '';
+}
+
+function getTodoIdValueFromAttr(itemThatWasClicked) {
+    return itemThatWasClicked.parentElement.getAttribute('data-todo-id');
+}
+
 
 
 
@@ -45,15 +73,17 @@ function render() {
 
         // terenary operator 
         //               if    ?   kai true   :  kai false 
-        classCheck = todo.done ? 'fa-check-circle' : 'fa-circle-thin';
-        textChecked = todo.done ? 'line-through' : '';
+        // classCheck = todo.done ? 'fa-check-circle' : 'fa-circle-thin';
+        // textChecked = todo.done ? 'line-through' : '';
         // if (todo.done) textChecked = 'line-through';
+
+
 
 
         listElement.innerHTML += `
         <li class="item" data-todo-id="${todoId}">
-            <i class="fa ${classCheck}" aria-hidden="true"></i>
-            <span class="text ${textChecked}">${todo.name}</span>
+            <i class="fa ${handleClassCheck(todo)} check-box" aria-hidden="true"></i>
+            ${renderSpanOrInput(todo)}
             <i class="fa fa-pencil-square" aria-hidden="true"></i>
             <i class="fa fa-trash" aria-hidden="true"></i>
         </li>
@@ -97,11 +127,7 @@ inputElement.addEventListener('keyup', function(event) {
 
     if (event.keyCode === 13) {
         // buvo paspaustas enter
-        let inputVal = inputElement.value;
-        // console.log(inputVal);
-        //trumpa versija
-        if (inputVal !== '') addTodo(inputVal);
-        inputElement.value = '';
+        addValueToTodoList(inputElement.value);
     }
 })
 
@@ -114,10 +140,58 @@ inputElement.addEventListener('keyup', function(event) {
 listElement.addEventListener('click', function(event) {
     // console.log(event.target);
     let elemtetWasPressed = event.target;
+
+    // elemento istrynimas ====================================
     if (elemtetWasPressed.classList.contains('fa-trash')) {
         // console.log('trash pressed');
-        let todoIdValueFromAttr = elemtetWasPressed.parentElement.getAttribute('data-todo-id');
-        console.log(todoIdValueFromAttr);
-        deleteTodo(todoIdValueFromAttr);
+        // let todoIdValueFromAttr = elemtetWasPressed.parentElement.getAttribute('data-todo-id');
+        // console.log(todoIdValueFromAttr);
+        // funkcijos kvietinys 
+        // deleteTodo(todoIdValueFromAttr);
+        deleteTodo(getTodoIdValueFromAttr(elemtetWasPressed));
     }
+
+    // elemento check uncheck ====================================
+    // ar paspaudem ant scrituliuko clase  check-box
+    if (elemtetWasPressed.classList.contains('check-box')) {
+        // console.log('check');
+        let todoIdValueFromAttr = elemtetWasPressed.parentElement.getAttribute('data-todo-id');
+        // pakeisti paspausto objekto done reiksme i priesinga
+        // !true == false
+        todos[todoIdValueFromAttr].done = !todos[todoIdValueFromAttr].done;
+        render();
+    }
+
+    // elemento Edit ====================================
+
+    if (elemtetWasPressed.classList.contains('fa-pencil-square')) {
+        // console.log('pencil');
+
+        // surandame li ant kurio paspaudem
+        let todoIdValueFromAttr = elemtetWasPressed.parentElement.getAttribute('data-todo-id');
+
+        // pasitikrinam ar paspausta reiksme edit yra true
+        if (todos[todoIdValueFromAttr].edit) {
+            // elmento edit yra true
+            // pakeisti input elemnta i span
+            console.log('pakeisti input elemnta i span');
+            // issisaugoti pakeisto input reiksme ir tada perkelti ja i elemnta
+            //    einam nuo paspausto edit el  i tevini el ir jame ieskom input elem reiksmes
+            let editedTodoText = elemtetWasPressed.parentElement.querySelector('input').value;
+            console.log(editedTodoText);
+            todos[todoIdValueFromAttr].name = editedTodoText;
+            todos[todoIdValueFromAttr].edit = false;
+
+        } else {
+            // elmento edit yra false 
+            // pakeisti is span i input
+            console.log('pakeisti is span i input');
+            todos[todoIdValueFromAttr].edit = !todos[todoIdValueFromAttr].edit;
+
+        }
+        render();
+
+    }
+
+
 })
